5e01ca49949471288002662fc4cca22347277c5c
MyFRA <predimolanaz23@gmail.com>
2023-06-22 16:36:02 +0700

    feat: add Model Daily Attendance -mc

---
+++ app/Models/DailyAttendance.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DailyAttendance extends Model
+{
+    use Uuid, HasFactory;
+
+    protected $table = 'DAILY ATTENDANCES';
+
+    protected $fillable = [
+        'uuid',
+        'employee_uuid',
+        'date',
+        'presence_entry_status',
+        'presence_exit_status',
+        'presence_entry_address',
+        'presence_entry_latitude',
+        'presence_entry_longitude',
+        'presence_exit_address',
+        'presence_exit_latitude',
+        'presence_exit_longitude',
+        'reference_address',
+        'reference_latitude',
+        'reference_longitude',
+    ];
+
+    protected $primaryKey = 'uuid';
+}
---
+++ database/migrations/2023_06_22_080228_create_daily_attendances_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('DAILY_ATTENDANCES', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->uuid('employee_uuid');
+            $table->dateTime('date');
+            $table->enum('presence_entry_status', ['on_time', 'late', 'not_present'])->nullable();
+            $table->enum('presence_exit_status', ['on_time', 'early`', 'not_present'])->nullable();
+            $table->text('presence_entry_address');
+            $table->double('presence_entry_latitude');
+            $table->double('presence_entry_longitude');
+            $table->text('presence_exit_address');
+            $table->double('presence_exit_latitude');
+            $table->double('presence_exit_longitude');
+            $table->text('reference_address');
+            $table->double('reference_latitude');
+            $table->double('reference_longitude');
+            $table->timestamps();
+
+            $table->foreign('employee_uuid')->references('uuid')->on('EMPLOYEE')->onDelete('RESTRICT');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('DAILY_ATTENDANCES');
+    }
+};

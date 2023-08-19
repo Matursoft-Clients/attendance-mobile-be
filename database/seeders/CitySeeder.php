d5a59d6bcd22c9012a0be29c2618c8ef10a8a29b
To My <60420319+MyFRA@users.noreply.github.com>
2023-08-19 10:38:31 +0700

    Update CitySeeder.php

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -13,7 +13,7 @@ class CitySeeder extends Seeder
      */
     public function run(): void
     {
-        $json_cities = Storage::get('city/cities.json');
+        $json_cities = Storage::get('app/cities.json');
 
         $cities = json_decode($json_cities, true);
 
